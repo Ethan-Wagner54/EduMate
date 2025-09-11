edc51d867b60ce1e0340cfe0eb10c71a6ae98c31
Lebogang Shearaft Mavhungu <lebogangshearaft@gmail.com>
2025-09-11 20:55:49 +0200

    Update App.jsx

--- edumate-frontend/src/App.jsx
+++ edumate-frontend/src/App.jsx
@@ -1,7 +1,10 @@
+
+import { MessagingCenter } from "./components/messaging-center";
+
 export default function App() {
   return (
-    <h1 className="text-4xl font-bold text-blue-500">
-      Hello Tailwind
-    </h1>
-  )
+    <div className="h-screen w-full">
+      <MessagingCenter />
+    </div>
+  );
 }
